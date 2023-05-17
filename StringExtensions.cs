1b78efcb0c2a1d1295c10e1ab54f821d2b4ebd0b
Mattia Rombi <mattyro96@gmail.com>
2023-05-18 00:08:39 +0200

    Added extension to check if a string is empty
    This commit adds a new file, StringExtensions.cs, which contains an extension method for strings called IsEmpty. This method checks if a given string is null or whitespace and returns true if it is. The new file has been added to the project file Runner.csproj.

---
+++ StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Runner
+{
+    internal static class StringExtensions
+    {
+        public static bool IsEmpty(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
+    }
+}
