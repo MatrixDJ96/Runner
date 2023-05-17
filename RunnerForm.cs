02a4b0e7f989cacda485f3be490c80a647e6bda6
Mattia Rombi <mrombi@siatass.com>
2023-05-17 13:55:47 +0200

    Renamed main form name

--- RunnerForm.cs
+++ RunnerForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Runner
 {
-    public partial class Form1 : Form
+    public partial class RunnerForm : Form
     {
-        public Form1()
+        public RunnerForm()
         {
             InitializeComponent();
         }
