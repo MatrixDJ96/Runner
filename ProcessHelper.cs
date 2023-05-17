e4d98c194e38d09a90ecd67a8794a9166d7211a4
Mattia Rombi <mattyro96@gmail.com>
2023-05-18 00:10:12 +0200

    Added helped to manage process execution
    This commit adds a new file, CmdProcess.cs, which contains the implementation of a class that manages command line processes. The class has methods for creating, starting and killing processes as well as events for when the process starts and exits or when data is received from stdout or stderr.

---
+++ ProcessHelper.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Runner
+{
+    internal class ProcessHelper
+    {
+        public Process Process { get; private set; } = null;
+
+        public bool IsRunning { get => !(Process?.HasExited ?? true); }
+
+        // Event on process started
+        public event EventHandler Started;
+
+        // Event on process exited
+        public event EventHandler Exited;
+
+        // Event on process stderr data received
+        public event DataReceivedEventHandler ErrorDataReceived;
+
+        // Event on process stdout data received
+        public event DataReceivedEventHandler OutputDataReceived;
+
+        private void OnStarted()
+        {
+            if (Started != null)
+            {
+                // Invoke listners
+                Started(this, EventArgs.Empty);
+            }
+        }
+
+        public Process Create(string file)
+        {
+            if (Process == null)
+            {
+                // Generate working from file path
+                var workingDir = Path.GetDirectoryName(file);
+
+                // Create process with custom start info
+                Process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "cmd.exe",
+                        Arguments = "/c " + file,
+                        WorkingDirectory = workingDir,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        CreateNoWindow = true,
+                    }
+                };
+
+                // Set exited event listner
+                Process.Exited += Exited;
+                Process.Exited += (s, e) => Process = null;
+
+                // Set output events listners
+                Process.ErrorDataReceived += ErrorDataReceived;
+                Process.OutputDataReceived += OutputDataReceived;
+
+                // Enable process events
+                Process.EnableRaisingEvents = true;
+            }
+
+            return Process;
+        }
+
+        public bool Start()
+        {
+            var result = false;
+
+            if (Process != null)
+            {
+                try
+                {
+                    Process.Start();
+                    result = true;
+                }
+                catch { }
+
+                if (result)
+                {
+                    // Trigger event
+                    OnStarted();
+                    // Listen process output
+                    Process.BeginOutputReadLine();
+                    Process.BeginErrorReadLine();
+                }
+            }
+
+            return result;
+        }
+
+        public bool Kill()
+        {
+            var result = false;
+
+            if (Process != null)
+            {
+                try
+                {
+                    // Kill process
+                    Process.Kill();
+                    Process = null;
+                    result = true;
+                }
+                catch { }
+            }
+
+            return result;
+        }
+    }
+}
