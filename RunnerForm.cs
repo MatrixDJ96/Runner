e10791d5e4f5bafd2356d4ac55cb632e66111b11
Mattia Rombi <mattyro96@gmail.com>
2023-05-18 00:13:22 +0200

    Added method to update GUI components
    This commit refactors the RunnerForm.cs file by adding a new method UpdateComponents() that updates the start and stop buttons, as well as the file to execute label. The CmdProcess class is also instantiated in the constructor of RunnerForm. These changes improve the user interface and make it more responsive.

--- RunnerForm.cs
+++ RunnerForm.cs
@@ -1,20 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace Runner
 {
     public partial class RunnerForm : Form
     {
+        private ProcessHelper ProcessHelper { get; set; } = new ProcessHelper();
+
         public RunnerForm()
         {
             InitializeComponent();
+        }
+
+        private void UpdateComponents()
+        {
+            if (InvokeRequired)
+            {
+                // Invoke correct thread to update gui
+                Invoke((MethodInvoker)delegate { UpdateComponents(); });
+
+                return;
+            }
+
+            // Update start and stop button
+            StartButton.Enabled = !Settings.FileToExecute.IsEmpty() && !ProcessHelper.IsRunning;
+            StopButton.Enabled = !Settings.FileToExecute.IsEmpty() && ProcessHelper.IsRunning;
+
+            // Update file to execute label
+            FileToExecuteLabel.Text = Settings.FileToExecute;
+            FileToExecuteLabel.Enabled = StartButton.Enabled;
         }
     }
 }
