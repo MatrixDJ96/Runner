cfcebbf759e4c3ca079f54935b09845e91d20a47
Mattia Rombi <mrombi@siatass.com>
2023-05-19 09:50:16 +0200

    Removed auto-sizing from runner form
    The commit removes the auto-sizing feature from RunnerForm, which was causing issues with the layout of the form. The form will now have a fixed size of 800x450 pixels.

--- RunnerForm.Designer.cs
+++ RunnerForm.Designer.cs
@@ -162,7 +162,6 @@ namespace Runner
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.FillPanel);
             this.Controls.Add(this.TopPanel);
